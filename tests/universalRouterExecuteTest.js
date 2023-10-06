ca71a56d668124adb3dc6d894a9302c1e61bed61
Hiroyuki Naito <hiroyukinaito.eth@gmail.com>
2023-10-06 17:37:47 +0900

    Addted test cases and changed test file name #5

--- tests/universalRouterExecuteTest.js
+++ tests/universalRouterExecuteTest.js
@@ -15,7 +15,7 @@ const {
 // Uniswap Command Test
 describe("Getting Commands", () => {
   const testFile = JSON.parse(
-    fs.readFileSync("tests/V2_SWAP_EXACT_IN_EXECUTE.json", "utf-8")
+    fs.readFileSync("tests/0x0b08.json", "utf-8")
   );
   it("should have a hex string sequence", () => {
     let commandString = uniswapCommands(testFile.input);
@@ -32,7 +32,7 @@ describe("Getting Commands", () => {
 
 describe("Getting Inputs Data", () => {
   const testFile = JSON.parse(
-    fs.readFileSync("tests/V2_SWAP_EXACT_IN_EXECUTE.json", "utf-8")
+    fs.readFileSync("tests/0x0b08.json", "utf-8")
   );
   it("should have input array", () => {
     let inputArray = uniswapInputArray(testFile.input);
@@ -69,7 +69,7 @@ describe("Getting Inputs Data", () => {
 describe("Getting Deadline", () => {
   it("should have deadline ", () => {
     const testFile = JSON.parse(
-      fs.readFileSync("tests/V3_SWAP_EXACT_OUT_EXECUTE.json", "utf-8")
+      fs.readFileSync("tests/0x0b010c.json", "utf-8")
     );
     console.log("Actual V3 Array:", uniswapDeadline(testFile.input));
     expect(uniswapDeadline(testFile.input)).to.eql(1674344111n);
@@ -79,7 +79,7 @@ describe("Getting Deadline", () => {
 describe("Getting V3 Transaction data", () => {
   it("should correctly translate V3 path bytes to address", () => {
     const testFile = JSON.parse(
-      fs.readFileSync("tests/V3_SWAP_EXACT_OUT_EXECUTE.json", "utf-8")
+      fs.readFileSync("tests/0x0b010c.json", "utf-8")
     );
     let decodedV3InputArray = uniswapV3DecodedInputArray(testFile.input);
     console.log("Actual V3 Array:", decodedV3InputArray);
@@ -105,7 +105,7 @@ describe("Getting V3 Transaction data", () => {
 describe("Transaction processing", () => {
   it("should correctly identify and decode a V2 swap exact in transaction from execute", () => {
     const testFile = JSON.parse(
-      fs.readFileSync("tests/V2_SWAP_EXACT_IN_EXECUTE.json", "utf-8")
+      fs.readFileSync("tests/0x0b08.json", "utf-8")
     );
     let fullDecodedInput = uniswapFullDecodedInput(testFile.input);
     console.log(
@@ -148,7 +148,7 @@ describe("Transaction processing", () => {
 
   it("should correctly identify and decode a V2 swap exact out transaction from execute", () => {
     const testFile = JSON.parse(
-      fs.readFileSync("tests/V2_SWAP_EXACT_OUT_EXECUTE.json", "utf-8")
+      fs.readFileSync("tests/0x0b090c.json", "utf-8")
     );
     let fullDecodedInput = uniswapFullDecodedInput(testFile.input);
     console.log(
@@ -199,7 +199,7 @@ describe("Transaction processing", () => {
 
   it("should correctly identify and decode a V3 swap exact in transaction from execute", () => {
     const testFile = JSON.parse(
-      fs.readFileSync("tests/V3_SWAP_EXACT_IN_EXECUTE.json", "utf-8")
+      fs.readFileSync("tests/0x0b00.json", "utf-8")
     );
 
     let fullDecodedInput = uniswapFullDecodedInput(testFile.input);
@@ -244,7 +244,7 @@ describe("Transaction processing", () => {
 
   it("should correctly identify and decode a V3 swap exact in transaction from execute", () => {
     const testFile = JSON.parse(
-      fs.readFileSync("tests/V3_SWAP_EXACT_OUT_EXECUTE.json", "utf-8")
+      fs.readFileSync("tests/0x0b010c.json", "utf-8")
     );
 
     let fullDecodedInput = uniswapFullDecodedInput(testFile.input);
@@ -292,6 +292,143 @@ describe("Transaction processing", () => {
           ]
         ],
         deadline: 1674344111n
+      });
+  });
+  it("should correctly identify and decode a permit2 permit transaction from execute", () => {
+    const testFile = JSON.parse(
+      fs.readFileSync("tests/0x0a000c.json", "utf-8")
+    );
+
+    let fullDecodedInput = uniswapFullDecodedInput(testFile.input);
+    console.log(
+      util.inspect(fullDecodedInput, false, null, true /* enable colors */)
+    );
+    expect(fullDecodedInput).to.eql({
+        contents: [
+          [
+            {
+              command: '0a',
+              value: 'PERMIT2_PERMIT',
+              inputType: [
+                'tuple((address,uint160,uint48,uint48),address,uint256)',
+                'bytes'
+              ],
+              decodedInput: [
+                [
+                  [
+                    '0x3B960E47784150F5a63777201ee2B15253D713e8',
+                    1461501637330902918203684832716283019655932542975n,
+                    1699172129n,
+                    0n
+                  ],
+                  '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD',
+                  1696581929n
+                ],
+                '0x2a3fe6b6af8c46505658c48be8bebb4b22588d0d605cdf8ed22461bcc03adc1b480ee0b6deb7354889e702506dbf703c83b1aab3a8a9d774283727b6455721b51c'
+              ]
+            }
+          ],
+          [
+            {
+              command: '00',
+              value: 'V3_SWAP_EXACT_IN',
+              inputType: [ 'address', 'uint256', 'uint256', 'bytes', 'bool' ],
+              decodedInput: [
+                '0x0000000000000000000000000000000000000002',
+                14018556769268767n,
+                11001239826448700n,
+                [
+                  '0x3B960E47784150F5A63777201EE2B15253D713E8',
+                  500n,
+                  '0xA0B86991C6218B36C1D19D4A2E9EB0CE3606EB48',
+                  500n,
+                  '0xC02AAA39B223FE8D0A0E5C4F27EAD9083C756CC2'
+                ],
+                true
+              ]
+            }
+          ],
+          [
+            {
+              command: '0c',
+              value: 'UNWRAP_WETH',
+              inputType: [ 'address', 'uint256' ],
+              decodedInput: [
+                '0x0000000000000000000000000000000000000001',
+                11001239826448700n
+              ]
+            }
+          ]
+        ],
+        deadline: 1696581923n
+      });
+  });
+
+  it("should correctly identify and decode a payportion and a sweep transaction from execute", () => {
+    const testFile = JSON.parse(
+      fs.readFileSync("tests/0x0b080604.json", "utf-8")
+    );
+
+    let fullDecodedInput = uniswapFullDecodedInput(testFile.input);
+    console.log(
+      util.inspect(fullDecodedInput, false, null, true /* enable colors */)
+    );
+    expect(fullDecodedInput).to.eql({
+        contents: [
+          [
+            {
+              command: '0b',
+              value: 'WRAP_ETH',
+              inputType: [ 'address', 'uint256' ],
+              decodedInput: [
+                '0x0000000000000000000000000000000000000002',
+                5000000000000000n
+              ]
+            }
+          ],
+          [
+            {
+              command: '08',
+              value: 'V2_SWAP_EXACT_IN',
+              inputType: [ 'address', 'uint256', 'uint256', 'address[]', 'bool' ],
+              decodedInput:  [
+                '0x0000000000000000000000000000000000000002',
+                5000000000000000n,
+                11281527462407831653458207n,
+                [
+                  '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+                  '0x6982508145454Ce325dDbE47a25d4ec3d2311933'
+                ],
+                false
+              ]
+            }
+          ],
+          [
+            {
+              command: '06',
+              value: 'PAY_PORTION',
+              inputType: [ 'address', 'address', 'uint256' ],
+              decodedInput: [
+                '0x6982508145454Ce325dDbE47a25d4ec3d2311933',
+                '0x17CC6042605381c158D2adab487434Bde79Aa61C',
+                100n
+              ]
+            }
+          ],
+          [
+            {
+              command: '04',
+              value: 'SWEEP',
+              inputType: [ 'address', 'address', 'uint256' ],
+              decodedInput:  [
+                '0x6982508145454Ce325dDbE47a25d4ec3d2311933',
+                '0x0000000000000000000000000000000000000001',
+                11281527462407831653458207n
+              ]
+            }
+          ]
+        ],
+        deadline: 1696581227n
       });
   });
 });
